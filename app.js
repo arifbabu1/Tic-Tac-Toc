38c2d2080664aa2088329ef9b18617be0000d780
Arif Babu <babuarif5566@gmail.com>
2024-08-12 20:15:07 +0600

    Add files via upload
    adding all basic file

---
+++ app.js
@@ -0,0 +1,20 @@
+let boxbtn=document.querySelector(".box")
+let newgamebtn=document.querySelector("#new")
+let resetbtn=document.querySelector("#reset")
+let game=document.querySelector(".game")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
