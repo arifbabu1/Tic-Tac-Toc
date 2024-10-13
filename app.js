a7530553f12cd8e665d3de40d2d965fc92bc1c30
Arif Babu <babuarif5566@gmail.com>
2024-10-14 00:53:16 +0600

    Update app.js
    removing the code

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-let boxbtn=document.querySelector(".box")
-let newgamebtn=document.querySelector("#new")
-let resetbtn=document.querySelector("#reset")
-let game=document.querySelector(".game")
 
 
 
